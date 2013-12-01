22e58e9544ccc3f3489201c1ab2b1677c8e605f1
Cristiano Faustino <cristiadu@gmail.com>
2013-12-01 21:03:08 -0200

    Add ShoppingOrderItem

--- grails-app/domain/br/ufscar/isell/ShoppingOrder.groovy
+++ grails-app/domain/br/ufscar/isell/ShoppingOrder.groovy
@@ -5,7 +5,7 @@ class ShoppingOrder {
     Client client
     Date createdAt = new Date()
 
-    static hasMany = [ products: Product, payments: Payment ]
+    static hasMany = [ payments: Payment ]
     static belongsTo = Product
 
     static constraints = {
